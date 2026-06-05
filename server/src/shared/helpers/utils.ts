e3ecf23319dd7f4b5f1b8dca4bc8d438fbb46418
Martin Tichý <147166020+PepaPanda@users.noreply.github.com>
2026-06-05 20:56:54 +0200

    fix spammy notifications and divide logic into helpers (#36)

--- server/src/shared/helpers/utils.ts
+++ server/src/shared/helpers/utils.ts
@@ -9,3 +9,7 @@ export const isObjectEmpty = (objectName: object) => {
 export const toTime = (value: Date | string): number => {
   return value instanceof Date ? value.getTime() : new Date(value).getTime();
 };
+
+export const average = (values: number[]): number => {
+    return values.reduce((sum, value) => sum + value, 0) / values.length;
+};
